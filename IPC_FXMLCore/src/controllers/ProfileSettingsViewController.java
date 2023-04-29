f452c087aefd134c2e8e56a7cc34cd60d5409552
Lucasarl <90321541+Lucasarl@users.noreply.github.com>
2023-04-29 13:13:56 +0200

    Profile settings (view only)

---
+++ IPC_FXMLCore/src/controllers/ProfileSettingsViewController.java
@@ -0,0 +1,66 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package controllers;
+
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import model.*;
+
+/**
+ * FXML Controller class
+ *
+ * @author USER
+ */
+public class ProfileSettingsViewController implements Initializable {
+
+    @FXML
+    private Label name;
+    @FXML
+    private Label familyName;
+    @FXML
+    private Label password;
+    @FXML
+    private Label nickname;
+    @FXML
+    private Label cardNumber;
+    @FXML
+    private Label svc;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb){
+        // TODO2
+        Image avatar=null;
+        try {
+        Club c=Club.getInstance();
+        c.setInitialData(); //REINICIA LOS DATOS DEL CLUB
+        String nickName="Ntonio";
+        String passwordMember="erewrqdc";
+        c.registerMember("Pedro","Antonio Palillo","643213454","Ntonio","erewrqdc","5402056301030199",321,avatar); 
+        Member m=c.getMemberByCredentials(nickName,passwordMember);
+        System.out.println(c.existsLogin("Ntonio"));
+        name.textProperty().setValue(m.getName());
+        familyName.textProperty().setValue(m.getSurname());
+        password.textProperty().setValue(m.getPassword());
+        nickname.textProperty().setValue(m.getNickName());
+        cardNumber.textProperty().setValue(m.getCreditCard());
+        String stringSvc=Integer.toString(m.getSvc());
+        svc.textProperty().setValue(stringSvc);
+        
+        }
+        
+        catch(Exception e) {
+            
+        }
+    }    
+    
+}
--- IPC_FXMLCore/src/ipc_fxmlcore/IPC_FXMLCore.java
+++ IPC_FXMLCore/src/ipc_fxmlcore/IPC_FXMLCore.java
@@ -1,20 +1,41 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
+ 
 package ipc_fxmlcore;
 
-/**
- *
- * @author USER
- */
-public class IPC_FXMLCore {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class IPC_FXMLCore extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        //======================================================================
+        // 1- creación del grafo de escena a partir del fichero FXML
+        FXMLLoader loader= new  FXMLLoader(getClass().getResource("/views/profileSettingsView.fxml"));
+        Parent root = loader.load();
+        //======================================================================
+        // 2- creación de la escena con el nodo raiz del grafo de escena
+        Scene scene = new Scene(root);
+        //======================================================================
+        // 3- asiganación de la escena al Stage que recibe el metodo 
+        //     - configuracion del stage
+        //     - se muestra el stage de manera no modal mediante el metodo show()
+        stage.setScene(scene);
+        stage.setTitle("profileSettingsView");
+        stage.show();
+    }
+    
+     
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        launch(args);
+        
     }
-    
 }
